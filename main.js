64b2722d2219b17d3cc5e5274227a8e424d21ced
HippErger <berger.clayton@gmail.com>
2020-02-10 17:22:52 -0600

    added base code

--- main.js
+++ main.js
@@ -1,71 +1,111 @@
 'use strict';
 
-// brings in the assert module for unit testing
 const assert = require('assert');
-// brings in the readline module to access the command line
 const readline = require('readline');
-// use the readline module to print out to the command line
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 
-const pigLatin = (word) => {
-
+function Checker() {
   // Your code here
-
 }
 
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
+class Board {
+  constructor() {
+    this.grid = []
+  }
+  // method that creates an 8x8 array, filled with null values
+  createGrid() {
+    // loop to create the 8 rows
+    for (let row = 0; row < 8; row++) {
+      this.grid[row] = [];
+      // push in 8 columns of nulls
+      for (let column = 0; column < 8; column++) {
+        this.grid[row].push(null);
+      }
+    }
+  }
+  viewGrid() {
+    // add our column numbers
+    let string = "  0 1 2 3 4 5 6 7\n";
+    for (let row = 0; row < 8; row++) {
+      // we start with our row number in our array
+      const rowOfCheckers = [row];
+      // a loop within a loop
+      for (let column = 0; column < 8; column++) {
+        // if the location is "truthy" (contains a checker piece, in this case)
+        if (this.grid[row][column]) {
+          // push the symbol of the check in that location into the array
+          rowOfCheckers.push(this.grid[row][column].symbol);
+        } else {
+          // just push in a blank space
+          rowOfCheckers.push(' ');
+        }
+      }
+      // join the rowOfCheckers array to a string, separated by a space
+      string += rowOfCheckers.join(' ');
+      // add a 'new line'
+      string += "\n";
+    }
+    console.log(string);
+  }
+
+  // Your code here
+}
+
+class Game {
+  constructor() {
+    this.board = new Board;
+  }
+  start() {
+    this.board.createGrid();
+  }
+}
+
+function getPrompt() {
+  game.board.viewGrid();
+  rl.question('which piece?: ', (whichPiece) => {
+    rl.question('to where?: ', (toWhere) => {
+      game.moveChecker(whichPiece, toWhere);
+      getPrompt();
+    });
   });
 }
 
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
+const game = new Game();
+game.start();
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
+
+// Tests
+if (typeof describe === 'function') {
+  describe('Game', () => {
+    it('should have a board', () => {
+      assert.equal(game.board.constructor.name, 'Board');
     });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
+    it('board should have 24 checkers', () => {
+      assert.equal(game.board.checkers.length, 24);
     });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
+  });
+
+  describe('Game.moveChecker()', () => {
+    it('should move a checker', () => {
+      assert(!game.board.grid[4][1]);
+      game.moveChecker('50', '41');
+      assert(game.board.grid[4][1]);
+      game.moveChecker('21', '30');
+      assert(game.board.grid[3][0]);
+      game.moveChecker('52', '43');
+      assert(game.board.grid[4][3]);
     });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+    it('should be able to jump over and kill another checker', () => {
+      game.moveChecker('30', '52');
+      assert(game.board.grid[5][2]);
+      assert(!game.board.grid[4][1]);
+      assert.equal(game.board.checkers.length, 23);
     });
   });
 } else {
-
   getPrompt();
-
 }
-
-
-
-
-
-
-// **********
-//   HINTS
-// **********
-
-// break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
-// 2. if word begins in with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
